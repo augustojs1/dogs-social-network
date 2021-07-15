55f6f3cb0960e79921a0c4460a3fa82e94192818
Augusto Souza <augustojsouza1@gmail.com>
2021-07-15 20:21:03 -0300

    Protecting USer route to logged users only

---
+++ src/Helper/ProtectedRoute.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+import React from "react";
+import { UserContext } from "../UserContext";
+import { Navigate, Route } from "react-router-dom";
+
+const ProtectedRoute = () => {
+  const { login } = React.useContext(UserContext);
+
+  if (login === true) return <Route {...props} />;
+  else if (login === false) return <Navigate to="/login" />;
+  else return null;
+};
+
+export default ProtectedRoute;
