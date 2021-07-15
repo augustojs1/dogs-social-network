39196ec99f38b47861606bfdf02a9b3502db3a6b
Augusto Souza <augustojsouza1@gmail.com>
2021-07-15 19:36:24 -0300

    Fixed ESLint problems

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,30 +1,30 @@
-import React from 'react';
-import LoginForm from './LoginForm';
-import LoginCreate from './LoginCreate';
-import LoginPasswordLost from './LoginPasswordLost';
-import LoginPasswordReset from './LoginPasswordReset';
-import { Routes, Route } from 'react-router';
-import { UserContext } from '../../UserContext';
-import { Navigate } from 'react-router-dom';
-import styles from './Login.module.css';
+import React from "react";
+import LoginForm from "./LoginForm";
+import LoginCreate from "./LoginCreate";
+import LoginPasswordLost from "./LoginPasswordLost";
+import LoginPasswordReset from "./LoginPasswordReset";
+import { Routes, Route } from "react-router";
+import { UserContext } from "../../UserContext";
+import { Navigate } from "react-router-dom";
+import styles from "./Login.module.css";
 
 const Login = () => {
-    const { login } = React.useContext(UserContext);
+  const { login } = React.useContext(UserContext);
 
-    if(login === true) return <Navigate to="/conta" />
+  if (login === true) return <Navigate to="/conta" />;
 
-    return (
-        <section className={styles.login}>
-            <div className={styles.forms}>
-            <Routes>
-                <Route path="/" element={<LoginForm />} />
-                <Route path="criar" element={<LoginCreate />} />
-                <Route path="perdeu" element={<LoginPasswordLost />} />
-                <Route path="resetar" element={<LoginPasswordReset />} />
-            </Routes>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="criar" element={<LoginCreate />} />
+          <Route path="perdeu" element={<LoginPasswordLost />} />
+          <Route path="resetar" element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
+  );
+};
 
 export default Login;
