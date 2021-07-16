4fbc59b4f7f4149477040864d712b43de6519f59
Augusto Souza <augustojsouza1@gmail.com>
2021-07-16 19:38:18 -0300

    Finished the navigation for the logged users

--- src/Helper/ProtectedRoute.jsx
+++ src/Helper/ProtectedRoute.jsx
@@ -1,12 +1,12 @@
-/* eslint-disable no-undef */
 import React from "react";
 import { UserContext } from "../UserContext";
 import { Navigate, Route } from "react-router-dom";
+import User from "../Components/User/User";
 
 const ProtectedRoute = () => {
   const { login } = React.useContext(UserContext);
 
-  if (login === true) return <Route {...props} />;
+  if (login === true) return <Route path="conta/*" element={<User />} />;
   else if (login === false) return <Navigate to="/login" />;
   else return null;
 };
